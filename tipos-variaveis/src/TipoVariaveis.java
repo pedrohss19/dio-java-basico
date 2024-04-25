e88d8c109788850facca195e8a123f918436f004
Pedro Souza <pedrohsouzasouza@hotmail.com>
2024-04-25 01:13:17 -0400

    Tipos e variáveis

--- tipos-variaveis/src/TipoVariaveis.java
+++ tipos-variaveis/src/TipoVariaveis.java
@@ -1,5 +1,23 @@
 public class TipoVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        //Strings
+        String meuNome = "pedro";
+
+        System.out.println(meuNome);
+
+        //Declaração padrão de variavel
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.println(numero);
+
+        //Declaração de constante
+
+        final double VALOR_DE_PI = 3.14;
+
+        System.out.println(VALOR_DE_PI);
+
     }
 }
