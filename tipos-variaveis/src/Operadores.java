1e07ec67081e557a61475193dd11e7e4c1375efd
Pedro Souza <pedrohsouzasouza@hotmail.com>
2024-04-28 15:52:08 -0400

    exemplos de operadores em java

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,38 @@
+public class Operadores {
+    
+    public static void main(String[] args) {
+        /*
+        String concatenacao = "?";
+
+        concatenacao = 1+1+1 + "1";
+
+        System.out.println(concatenacao);
+         
+
+         int numero = 5;
+
+         System.out.println(- numero);
+        
+         //incremento
+         numero ++;
+
+         System.out.println(numero);
+        
+
+        //invertendo as variaveis dentro do print
+        boolean variavel = true;
+        System.out.println(!variavel);
+       
+
+        //Operador de Condição Ternária
+        int a, b;
+
+        a = 5;
+        b = 6;
+
+        String resultado  = (a==b) ? "verdadeiro" : "falso";
+
+        System.out.println(resultado);
+        */
+    }
+}
