7ef3e4881c65d827b827d921c7038e7af6eaae70
Pedro Souza <pedrohsouzasouza@hotmail.com>
2024-04-28 19:09:33 -0400

    Script para conta do banco

--- projetos/conta-banco/src/ContaTerminal.java
+++ projetos/conta-banco/src/ContaTerminal.java
@@ -1,5 +1,32 @@
+import java.util.*;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
+    /**
+     * @param args
+     */
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        int     numero;
+        String  agencia;
+        String  nomeCliente;
+        Float  saldo;
         
+        System.out.println("Por favor, digite o número da conta!");
+        numero = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Por favor, digite o número da agência!");
+        agencia = scanner.nextLine();
+
+        System.out.println("Por favor, digite o seu nome!");
+        nomeCliente = scanner.nextLine();
+
+        System.out.println("Por favor, digite o seu saldo!");
+        saldo = scanner.nextFloat();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque");
+
+        scanner.close();
     }
 }
